1e11f7146c27d8eb297f37f003667bcfbbeca8da
nefretac <karahanyavuz61@gmail.com>
2024-04-27 22:27:21 +0300

    successful connection to the database

--- src/RegistrationForm.java
+++ src/RegistrationForm.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class RegistrationForm extends JDialog {
     private JTextField tfName;
@@ -21,7 +22,8 @@ public class RegistrationForm extends JDialog {
         setMinimumSize(new Dimension(450, 474));
         setModal(true);
         setLocationRelativeTo(parent);
-        setVisible(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
         btnRegister.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -60,14 +62,64 @@ public class RegistrationForm extends JDialog {
                     JOptionPane.ERROR_MESSAGE);
             return;
         }
-        addUserToDatabase(name, email, phone, address, password);
+        user = addUserToDatabase(name, email, phone, address, password);
+        if (user != null) {
+            dispose();
+        }else {
+            JOptionPane.showMessageDialog(this,
+                    "Failed to register new user",
+                    "Try again.",
+                    JOptionPane.ERROR_MESSAGE);
+        }
     }
+
+    public User user;
     private User addUserToDatabase(String name, String email, String phone, String address, String password) {
         User user = null;
+        final String DB_URL = "jdbc:mysql://localhost/MyStore?serverTimezone=UTC";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
+
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            //CONNECT TO DATABASE
+
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO users (name, email, phone, address, password)" +
+                    "VALUES(?, ? , ? , ? , ? )";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, phone);
+            preparedStatement.setString(4, address);
+            preparedStatement.setString(5, password);
+
+            int addedRows =  preparedStatement.executeUpdate();
+            if(addedRows >0 ) {
+                user = new User();
+                user.name = name;
+                user.email = email;
+                user.phone = phone;
+                user.address = address;
+                user.password = password;
+            }
+
+            stmt.close();
+            conn.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return user;
     }
 
     public static void main(String[] args) {
         RegistrationForm myForm = new RegistrationForm(null);
+        User user = myForm.user;
+        if (user != null) {
+            System.out.println("Successful registration of: " + user.name);
+        } else {
+            System.out.println("Registration cancelled");
+        }
     }
 }
